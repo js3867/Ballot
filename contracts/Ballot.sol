437dd87c8cdc90ff3dbe5775913c8e289155eb78
js3867 <83634235+js3867@users.noreply.github.com>
2022-05-19 19:57:35 +0800

    continuation of codebase and fixed a couple of bugs, such as return objects, ABIEncoderV2 and return memory

--- contracts/Ballot.sol
+++ contracts/Ballot.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
+pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
@@ -42,6 +43,87 @@ contract Ballot is Ownable {
         }
     }
 
+    // cast vote by proposal name
+    function castVote(bytes32 _vote) public {
+        Voter storage sender = voters[msg.sender];
+
+        require(ballotIsOpen, "ballot is currently closed");
+        require(!ballotIsEnded, "ballot has already concluded");
+        require(!sender.voted, "address has already voted!");
+        require(sender.weight > 0, "address not approved to vote!");
+
+        uint256 check = 0;
+        for (uint256 i = 0; i < proposals.length; i++) {
+            if (proposals[i].name == _vote) {
+                proposals[i].voteCount += sender.weight;
+                check += 1;
+            }
+        }
+        if (check > 0) {
+            sender.voted = true;
+            sender.voteNo += 1;
+        }
+    }
+
+    // cast vote by index
+    function voteByIndex(uint256 _vote) public {
+        Voter storage sender = voters[msg.sender]; // define data location that will run between function calls
+
+        require(_vote <= proposals.length, "invalid entry");
+        require(ballotIsOpen, "ballot is currently closed");
+        require(!ballotIsEnded, "ballot has already concluded");
+        require(!sender.voted, "address has already voted!");
+        require(sender.weight > 0, "address not approved to vote!");
+
+        proposals[_vote].voteCount += sender.weight;
+
+        sender.voted = true;
+        sender.voteNo += 1;
+    }
+
+    // authentication
+    function giveRightToVote(address _voter) public onlyOwner {
+        require(!ballotIsEnded, "ballot has already concluded");
+        require(voters[_voter].weight == 0, "voter already has right to vote!");
+        require(!voters[_voter].voted, "voter has already voted!");
+
+        voters[_voter].weight == 1;
+    }
+
+    // display results only
+    function getProposalStatus(uint256 _proposal)
+        public
+        view
+        returns (Proposal memory proposals_)
+    {
+        return proposals[_proposal];
+    }
+
+    // end ballot and count votes
+    function endBallotAndDisplayResults()
+        public
+        onlyOwner
+        returns (bytes32 winner_, uint256 winningVoteCount_)
+    {
+        require(
+            ballotIsEnded == false,
+            "This ballot has already concluded. Please try getProposalsStatus() function"
+        );
+        require(ballotIsOpen == false, "ballot must be closed before ending");
+
+        bytes32 winner;
+        uint256 winningVoteCount = 0;
+
+        for (uint256 i = 0; i < proposals.length; i++) {
+            if (proposals[i].voteCount > winningVoteCount) {
+                winningVoteCount = proposals[i].voteCount;
+                winner = proposals[i].name;
+            }
+        }
+        ballotIsEnded = true;
+        return (winner, winningVoteCount);
+    }
+
     // is there a way to add approved addresses to authentication of Owner?
     function openBallot() public onlyOwner {
         require(ballotIsEnded == false, "This ballot has already concluded");
@@ -52,79 +134,6 @@ contract Ballot is Ownable {
     function closeBallot() public onlyOwner {
         require(ballotIsEnded == false, "ballot has already concluded");
         ballotIsOpen = false;
-    }
-
-    // cast vote
-    function castVote(bytes32 voteChoice) public {
-        Voter storage sender = voters[msg.sender];
-
-        require(ballotIsOpen, "ballot is currently closed");
-        require(!ballotIsEnded, "ballot has already concluded");
-        require(!sender.voted, "address has already voted!");
-        require(sender.weight > 0, "address not approved to vote!");
-
-        uint256 check = 0;
-        for (uint256 i = 0; i < proposalNames.length; i++) {
-            if (proposals[i] == voteChoice) {
-                proposals[i].voteCount += sender.weight;
-                check += 1;
-            }
-        }
-        if (check > 0) {
-            sender.voted = true;
-            // sender.voteNo = +1;
-        }
-    }
-
-    // cast vote by index
-    function voteByIndex(uint256 vote) public {
-        Voter storage sender = voters[msg.sender]; // define data location that will run between function calls
-
-        require(vote <= proposals.length, "invalid entry");
-        require(ballotIsOpen, "ballot is currently closed");
-        require(!ballotIsEnded, "ballot has already concluded");
-        require(!sender.voted, "address has already voted!");
-        require(sender.weight > 0, "address not approved to vote!");
-
-        proposals[vote].voteCount += sender.weight;
-
-        sender.voted = true;
-        sender.voteNo += 1;
-    }
-
-    // authentication
-    function giveRightToVote(address voter) public onlyOwner {
-        require(!ballotIsEnded, "ballot has already concluded");
-        require(voters[voter].weight == 0, "voter already has right to vote!");
-        require(!voters[voter].voted, "voter has already voted!");
-
-        voters[voter].weight == 1;
-    }
-
-    // display results only
-    function getProposalsStatus() public {
-        return proposals;
-    }
-
-    // end ballot and count votes
-    function endAndDisplayResults() public onlyOwner {
-        require(
-            ballotIsEnded == false,
-            "This ballot has already concluded. Please try getProposalsStatus() function"
-        );
-        require(ballotIsOpen == false, "ballot must be closed before ending");
-
-        bytes32 winner;
-        uint256 highest_votes = 0;
-
-        for (uint256 i = 0; i < proposalNames.length; i++) {
-            if (proposals[i].voteCount > highest_votes) {
-                highest_votes = proposals[i].voteCount;
-                winner = proposals[i].name;
-            }
-        }
-        ballotIsEnded = true;
-        return (winner, highest_votes);
     }
 }
 
